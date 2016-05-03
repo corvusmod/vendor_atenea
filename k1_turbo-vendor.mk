67223b4677fe3fa3adfc1f718581df867d94f4fb
ElTifo <tifo.el@gmail.com>
2016-05-03 10:35:11 +0100

    Rename to Aquaris_E5_FHD

--- k1_turbo-vendor.mk
+++ k1_turbo-vendor.mk
@@ -1,9 +1,9 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1_turbo/proprietary/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bq/Aquaris_E5_FHD/proprietary/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1_turbo/proprietary/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bq/Aquaris_E5_FHD/proprietary/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1_turbo/proprietary/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bq/Aquaris_E5_FHD/proprietary/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1_turbo/proprietary/xbin,system/xbin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bq/Aquaris_E5_FHD/proprietary/xbin,system/xbin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1_turbo/proprietary/vendor,system/vendor)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/bq/Aquaris_E5_FHD/proprietary/vendor,system/vendor)
