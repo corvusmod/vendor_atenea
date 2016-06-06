c74463fb1dceada4f313f87e8dae7bada233099d
Viktor Noskin <vitek2012rus@gmail.com>
2016-06-06 22:34:53 +0300

    Update HM2014011-vendor.mk

--- HM2014011-vendor.mk
+++ HM2014011-vendor.mk
@@ -1,10 +1,10 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Xiaomi/HM2014011/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/explay/atom/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Xiaomi/HM2014011/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/explay/atom/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Xiaomi/HM2014011/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/explay/atom/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Xiaomi/HM2014011/vendor,system/vendor)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/explay/atom/vendor,system/vendor)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Xiaomi/HM2014011/xbin,system/xbin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/explay/atom/xbin,system/xbin)
 
