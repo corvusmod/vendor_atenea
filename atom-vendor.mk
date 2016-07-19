1fcde0efa0ee4f2a5039e7c32a21073ad33b7b6e
Jesus Perez <jesus@silentcircle-com>
2016-07-19 18:31:39 +0200

    Change from atom to atenea

--- atom-vendor.mk
+++ atom-vendor.mk
@@ -1,10 +1,9 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/explay/atom/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/hexxa/atenea/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/explay/atom/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/hexxa/atenea/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/explay/atom/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/hexxa/atenea/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/explay/atom/vendor,system/vendor)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/hexxa/atenea/vendor,system/vendor)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/explay/atom/xbin,system/xbin)
-
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/hexxa/atenea/xbin,system/xbin)
